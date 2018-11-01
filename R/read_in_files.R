525693e087ee23c6a1b075994ebe42f9b3e007e0
Till Birkner <till.birkner@uni-potsdam.de>
2018-11-01 15:13:53 +0100

    implemented first functioning parallel processing of metadata table

--- R/read_in_files.R
+++ R/read_in_files.R
@@ -13,11 +13,34 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-CheckIndividualSignificance <- function(featureMat, metaMat, nnodes=1) {
-  cl<-makeForkCluster(nnodes = nnodes, outfile="")  # the parent process uses another core (so 4 cores will be used with this command)
-  registerDoParallel(cl)
-  parallelReturn = foreach(i= 1:n, .combine = 'rbind') %dopar% {
-    return(i)
+
+#' @export checkSufficientPower
+checkSufficientPower <- function(featureFile, metaFile, nnodes=1) {
+
+  metaMat <- read.table(file = metaFile, header = T, sep = "\t", row.names = 1)
+  covariates <- colnames (metaMat) [4:27] # each covariate + the status category, specific to example
+  noCovariates <- length (covariates)
+
+  conditionMat <- metaMat[metaMat$Case_status == "Schizo", ]
+  noCondition <- nrow(conditionMat)
+  noControl <- nrow(metaMat) - noCondition
+
+  write(paste("covariate", "control", "condition_negative", "condition_positive", sep = "\t"), file = "testfile.txt")
+  cl <- parallel::makeForkCluster(nnodes = nnodes, outfile="")  # the parent process uses another core (so 4 cores will be used with this command)
+  doParallel::registerDoParallel(cl)
+
+  parallelReturn <- foreach::foreach(i= 2:noCovariates, .combine = 'rbind') %dopar% {
+
+    aCovariate <- as.character (covariates [i])
+    condition <- table(eval(parse( text = as.character(paste0("schizoMat$", aCovariate)))))
+    noConditionNegative <- condition[1]
+    noConditionPositive <- condition[2]
+    write(paste(aCovariate, noControl, noConditionNegative, noConditionPositive, sep = "\t"), file = "testfile.txt", append = TRUE)
+    return(list(noquote(aCovariate), noControl, noConditionNegative, noConditionPositive))
+
   }
-  stopCluster(cl)
+
+  parallel::stopCluster(cl)
+  #return(as.data.frame(parallelReturn,stringsAsFactors=FALSE))
+  return(parallelReturn)
 }
