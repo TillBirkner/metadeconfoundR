aa025f26b282114d78dacc0e5bbbd4564be4b010
Till Birkner <till.birkner@uni-potsdam.de>
2018-11-06 13:21:37 +0100

    added metaFile format check and improved return structure

--- R/read_in_files.R
+++ R/read_in_files.R
@@ -15,9 +15,17 @@
 
 
 #' @export checkSufficientPower
-checkSufficientPower <- function(featureFile, metaFile, nnodes=1) {
+checkSufficientPower <- function(metaFile, nnodes=1, cutoff=5) {
 
-  metaMat <- read.table(file = metaFile, header = T, sep = "\t", row.names = 1)
+  metaMat <-
+    if (is.character(metaFile)) {
+      read.table(file = metaFile, header = T, sep = "\t", row.names = 1)
+    }
+    else if (is.data.frame(metaFile)) {
+      return(metaFile)
+    } else {
+      stop('Wrong metaFile argument! Needs to be either "path/to/file" or dataframe with row and column names.')
+    }
   covariates <- colnames (metaMat) [4:27] # each covariate + the status category, specific to example
   noCovariates <- length (covariates)
 
@@ -25,18 +33,49 @@ checkSufficientPower <- function(featureFile, metaFile, nnodes=1) {
   noCondition <- nrow(conditionMat)
   noControl <- nrow(metaMat) - noCondition
 
-  write(paste("covariate", "control", "condition_negative", "condition_positive", sep = "\t"), file = "testfile.txt")
+  write(paste("covariate",
+              "control",
+              "condition_negative",
+              "condition_positive",
+              sep = "\t"),
+        file = "testfile.txt")
   cl <- parallel::makeForkCluster(nnodes = nnodes, outfile="")  # the parent process uses another core (so 4 cores will be used with this command)
   doParallel::registerDoParallel(cl)
 
-  parallelReturn <- foreach::foreach(i= 2:noCovariates, .combine = 'rbind') %dopar% {
+  parallelReturn <- foreach::foreach(i= 2:noCovariates,
+                                     .combine = 'rbind',
+                                     .export = "conditionMat") %dopar% {
 
     aCovariate <- as.character (covariates [i])
-    condition <- table(eval(parse( text = as.character(paste0("schizoMat$", aCovariate)))))
+    condition <- table(eval
+                       (parse
+                         (text = as.character
+                           (paste0
+                             ("conditionMat$",
+                               aCovariate)))))  # count number of individuals having the phenotype and not taking/taking the covariate
     noConditionNegative <- condition[1]
     noConditionPositive <- condition[2]
-    write(paste(aCovariate, noControl, noConditionNegative, noConditionPositive, sep = "\t"), file = "testfile.txt", append = TRUE)
-    return(list(noquote(aCovariate), noControl, noConditionNegative, noConditionPositive))
+    robustCombination <- 0
+    if ((noControl >= cutoff) &&
+        (noConditionNegative >= cutoff) &&
+        (noConditionPositive >= cutoff)) {
+      robustCombination <- 1
+    }
+    write(paste
+          (aCovariate,
+            noControl,
+            noConditionNegative,
+            noConditionPositive,
+            robustCombination,
+            sep = "\t"),
+          file = "testfile.txt",
+          append = TRUE)
+    return(data.frame
+           (row.names = aCovariate,
+             noControl,
+             noConditionNegative,
+             noConditionPositive,
+             robustCombination))
 
   }
 
