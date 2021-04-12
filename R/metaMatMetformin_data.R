17f8840812ede3573e6b9c9d8330c2a5409f4e83
Birkner <tbirkne@cl-forslund07.mdc-berlin.net>
2021-04-12 14:50:18 +0200

    Version 0.2.0   - new function ImportLongPrior() to easily import prior knowledge of feature   metadata associations present in the current dataset.   - slightly improved help pages

--- R/metaMatMetformin_data.R
+++ R/metaMatMetformin_data.R
@@ -1,7 +1,8 @@
 #' Documentation for the metaMatMetformin RData in /data
 #'
-#' set of features from the metformin dataset,
-#' containining status for 5 dfferent proprties for 753 samples
+#' set of features from the metformin dataset
+#' (Forslund et al. (2015), DOI: https://doi.org/10.1038/nature15766),
+#' containing status for 5 different properties for 753 samples
 #'
 #'@name metaMatMetformin
 #'
